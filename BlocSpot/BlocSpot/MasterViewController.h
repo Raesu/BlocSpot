dab69d8e6fcd0d7a8b53090a65c352f4e220c1af
Ryan Summe <ryan.summe@gmail.com>
2015-07-21 18:46:25 -0400

    initial commit

---
+++ BlocSpot/BlocSpot/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  BlocSpot
+//
+//  Created by Ryan Summe on 7/21/15.
+//  Copyright (c) 2015 Ryan Summe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
